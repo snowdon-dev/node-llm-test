2676feb8ce132c3c0894b01979fe002ff615abe7
snowdon.dev <hello@snowdon.dev>
2025-07-11 14:03:48 +0100

    Add input len to gen words.

--- src/genwords.ts
+++ src/genwords.ts
@@ -1,6 +1,20 @@
 import { getRandomWords } from "./randomfile";
 
-getRandomWords(200).then((words) => {
+const args = process.argv.slice(2);
+const idx = args.indexOf('--number');
+
+if (idx === -1 || !args[idx+1]) {
+  console.error('Usage: node index.js --number <value>');
+  process.exit(1);
+}
+
+const num = parseInt(args[idx+1], 10);
+if (Number.isNaN(num)) {
+  console.error('Error: --number must be a valid integer');
+  process.exit(1);
+}
+
+getRandomWords(num).then((words) => {
   const res = JSON.stringify(words);
   console.log(res);
 });
